52933ed1552800feb8a29f9eefd520c38b54a39d
Jos Dehaes <jos.dehaes@gmail.com>
2025-02-07 16:00:59 +0100

    initial polkavm stuff

---
+++ language/polkavm/examples/basic/sources/morebasic.move
@@ -0,0 +1,6 @@
+module 0xa000::morebasic {
+
+    public entry fun sum(a: u64, b: u64): u64 {
+        a + b
+    }
+}
---
+++ language/polkavm/move-to-polka/src/main.rs
@@ -0,0 +1,20 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+#![forbid(unsafe_code)]
+
+use clap::Parser;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
+use move_to_polka::{options::Options, run_to_polka};
+
+fn main() -> anyhow::Result<()> {
+    let options = Options::parse();
+    let color = if atty::is(atty::Stream::Stderr) && atty::is(atty::Stream::Stdout) {
+        ColorChoice::Auto
+    } else {
+        ColorChoice::Never
+    };
+    let mut error_writer = StandardStream::stderr(color);
+    run_to_polka(&mut error_writer, options)
+}
