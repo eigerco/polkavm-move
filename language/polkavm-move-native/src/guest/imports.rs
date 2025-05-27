4e20f7736fe4a51e42800933f5ca0fc8c1a5eb09
Jos Dehaes <jos.dehaes@gmail.com>
2025-05-27 12:48:46 +0200

    wip: define hash functions

--- language/polkavm-move-native/src/guest/imports.rs
+++ language/polkavm-move-native/src/guest/imports.rs
@@ -1,4 +1,4 @@
-use crate::types::{AnyValue, MoveType};
+use crate::types::{AnyValue, MoveByteVector, MoveType};
 
 #[polkavm_derive::polkavm_import]
 extern "C" {
@@ -8,4 +8,14 @@ extern "C" {
 #[polkavm_derive::polkavm_import]
 extern "C" {
     pub(crate) fn debug_print(t: *const MoveType, v: *const AnyValue);
+}
+
+#[polkavm_derive::polkavm_import]
+extern "C" {
+    pub(crate) fn hash_sha2_256(t: *const MoveByteVector);
+}
+
+#[polkavm_derive::polkavm_import]
+extern "C" {
+    pub(crate) fn hash_sha3_256(t: *const MoveByteVector);
 }
--- language/polkavm-move-native/src/guest/mod.rs
+++ language/polkavm-move-native/src/guest/mod.rs
@@ -1,4 +1,4 @@
-use crate::types::{AnyValue, MoveAddress, MoveSigner, MoveType};
+use crate::types::{AnyValue, MoveAddress, MoveByteVector, MoveSigner, MoveType};
 
 mod allocator;
 mod imports;
@@ -15,9 +15,22 @@ unsafe extern "C" fn move_rt_vec_empty() {}
 #[export_name = "move_rt_vec_copy"]
 unsafe extern "C" fn move_rt_vec_copy() {}
 
+#[export_name = "move_rt_vec_cmp_eq"]
+unsafe extern "C" fn move_rt_vec_cmp_eq() {}
+
 #[export_name = "move_native_debug_print"]
 unsafe extern "C" fn print(type_x: *const MoveType, x: *const AnyValue) {
     imports::debug_print(type_x, x);
+}
+
+#[export_name = "move_native_hash_sha2_256"]
+unsafe extern "C" fn move_native_hash_sha2_256(bytes: MoveByteVector) {
+    imports::hash_sha2_256(&bytes);
+}
+
+#[export_name = "move_native_hash_sha3_256"]
+unsafe extern "C" fn move_native_hash_sha3_256(bytes: MoveByteVector) {
+    imports::hash_sha3_256(&bytes);
 }
 
 #[export_name = "move_native_signer_borrow_address"]
