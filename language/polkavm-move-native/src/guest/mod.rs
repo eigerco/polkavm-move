0012ede10ef555d97091d431b2da7daa4b98af3a
Jos Dehaes <jos.dehaes@gmail.com>
2025-05-27 12:33:19 +0200

    wip: define vec natives

--- language/polkavm-move-native/src/guest/mod.rs
+++ language/polkavm-move-native/src/guest/mod.rs
@@ -12,6 +12,12 @@ unsafe extern "C" fn move_rt_abort(code: u64) {
     imports::abort(code);
 }
 
+#[export_name = "move_rt_vec_empty"]
+unsafe extern "C" fn move_rt_vec_empty() {}
+
+#[export_name = "move_rt_vec_copy"]
+unsafe extern "C" fn move_rt_vec_copy() {}
+
 #[export_name = "move_native_debug_print"]
 unsafe extern "C" fn print(type_x: *const MoveType, x: *const AnyValue) {
     imports::debug_print(type_x, x);
