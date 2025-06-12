723630ad9ffbed18bf7e28a5aebbbe8c4e14f4e0
Jos Dehaes <jos.dehaes@gmail.com>
2025-06-12 11:26:26 +0200

    feat: run tests in parallel when using the same move source

--- crates/move-to-polka/tests/returns.rs
+++ crates/move-to-polka/tests/returns.rs
@@ -12,66 +12,62 @@ fn create_blob_once() -> ProgramBlob {
         .get_or_init(|| {
             initialize_logger();
             create_blob(
-                "output/string.polkavm",
-                "../../examples/basic/sources/string.move",
+                "output/returns.polkavm",
+                "../../examples/basic/sources/returns.move",
                 vec![],
             )
             .expect("Failed to compile Move source to PolkaVM bytecode")
         })
         .clone()
 }
-
 #[test]
-pub fn test_string() -> anyhow::Result<()> {
+pub fn test_rv_bool() -> anyhow::Result<()> {
     let blob = create_blob_once();
     let (mut instance, mut allocator) = create_instance(blob)?;
-    instance
-        .call_typed_and_get_result::<(), ()>(&mut allocator, "foo", ())
+    let result = instance
+        .call_typed_and_get_result::<u32, ()>(&mut allocator, "rv_bool_false", ())
         .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+    assert_eq!(result, 0);
+    let result = instance
+        .call_typed_and_get_result::<u32, ()>(&mut allocator, "rv_bool_true", ())
+        .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+    assert_eq!(result, 1);
 
     Ok(())
 }
 
 #[test]
-pub fn test_string_index_of() -> anyhow::Result<()> {
+pub fn test_rv_u16() -> anyhow::Result<()> {
     let blob = create_blob_once();
     let (mut instance, mut allocator) = create_instance(blob)?;
-    instance
-        .call_typed_and_get_result::<u64, ()>(&mut allocator, "index_of", ())
+    let result = instance
+        .call_typed_and_get_result::<u32, ()>(&mut allocator, "rv_u16", ())
         .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+    assert_eq!(result, 19);
 
     Ok(())
 }
 
 #[test]
-pub fn test_string_substring() -> anyhow::Result<()> {
+pub fn test_rv_u32() -> anyhow::Result<()> {
     let blob = create_blob_once();
     let (mut instance, mut allocator) = create_instance(blob)?;
-    instance
-        .call_typed_and_get_result::<(), ()>(&mut allocator, "substring", ())
+    let result = instance
+        .call_typed_and_get_result::<u32, ()>(&mut allocator, "rv_u32", ())
         .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+    assert_eq!(result, 19);
 
     Ok(())
 }
 
 #[test]
-pub fn test_append() -> anyhow::Result<()> {
+pub fn test_rv_u8() -> anyhow::Result<()> {
     let blob = create_blob_once();
     let (mut instance, mut allocator) = create_instance(blob)?;
-    instance
-        .call_typed_and_get_result::<u32, ()>(&mut allocator, "append", ())
+    let result = instance
+        .call_typed_and_get_result::<i32, ()>(&mut allocator, "rv_u8", ())
         .map_err(|e| anyhow::anyhow!("{e:?}"))?;
-
-    Ok(())
-}
-
-#[test]
-pub fn test_insert() -> anyhow::Result<()> {
-    let blob = create_blob_once();
-    let (mut instance, mut allocator) = create_instance(blob)?;
-    instance
-        .call_typed_and_get_result::<u32, ()>(&mut allocator, "insert", ())
-        .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+    assert_eq!(result, 19);
 
     Ok(())
 }
---
+++ crates/move-to-polka/tests/serialize.rs
@@ -0,0 +1,54 @@
+use move_to_polka::{
+    initialize_logger,
+    linker::{create_blob, create_instance},
+};
+use once_cell::sync::OnceCell;
+use polkavm::ProgramBlob;
+use polkavm_move_native::types::{MoveAddress, MoveSigner, ACCOUNT_ADDRESS_LENGTH};
+
+static COMPILE_ONCE: OnceCell<ProgramBlob> = OnceCell::new();
+
+fn create_blob_once() -> ProgramBlob {
+    COMPILE_ONCE
+        .get_or_init(|| {
+            initialize_logger();
+            create_blob(
+                "output/serialize.polkavm",
+                "../../examples/basic/sources/serialize.move",
+                vec![],
+            )
+            .expect("Failed to compile Move source to PolkaVM bytecode")
+        })
+        .clone()
+}
+
+#[test]
+pub fn test_serialize_string() -> anyhow::Result<()> {
+    let blob = create_blob_once();
+    let (mut instance, mut allocator) = create_instance(blob)?;
+    instance
+        .call_typed_and_get_result::<u32, ()>(&mut allocator, "ser_string", ())
+        .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+
+    Ok(())
+}
+
+#[test]
+pub fn test_serialize_signer() -> anyhow::Result<()> {
+    let blob = create_blob_once();
+    let (mut instance, mut allocator) = create_instance(blob)?;
+    let mut address_bytes = [1u8; ACCOUNT_ADDRESS_LENGTH];
+    // set markers for debug displaying
+    address_bytes[0] = 0xab;
+    address_bytes[ACCOUNT_ADDRESS_LENGTH - 1] = 0xce;
+
+    let move_signer = MoveSigner(MoveAddress(address_bytes));
+
+    let signer_address = allocator.copy_to_guest(&mut instance, &move_signer)?;
+
+    instance
+        .call_typed_and_get_result::<u32, (u32,)>(&mut allocator, "ser_signer", (signer_address,))
+        .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+
+    Ok(())
+}
