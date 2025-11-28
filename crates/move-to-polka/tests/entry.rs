1d28da75c4419ea66ca7235ab50c1159e3deb929
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-29 00:57:20 +0100

    wip: re-enable call selector

--- crates/move-to-polka/tests/entry.rs
+++ crates/move-to-polka/tests/entry.rs
@@ -1,11 +1,11 @@
-use std::collections::HashSet;
-
 use move_to_polka::{
     initialize_logger,
-    linker::{create_blob, create_instance},
+    linker::{copy_to_guest, create_blob, create_instance},
 };
 use once_cell::sync::OnceCell;
 use polkavm::ProgramBlob;
+use polkavm_move_native::types::{MoveAddress, MoveSigner, ACCOUNT_ADDRESS_LENGTH};
+use std::collections::HashSet;
 
 static COMPILE_ONCE: OnceCell<ProgramBlob> = OnceCell::new();
 
@@ -27,8 +27,17 @@ fn create_blob_once() -> ProgramBlob {
 pub fn entry() -> anyhow::Result<()> {
     let blob = create_blob_once();
     let (mut instance, mut runtime) = create_instance(blob)?;
+    let mut address_bytes = [1u8; ACCOUNT_ADDRESS_LENGTH];
+    // set markers for debug displaying
+    address_bytes[0] = 0xab;
+    address_bytes[ACCOUNT_ADDRESS_LENGTH - 1] = 0xce;
+
+    let move_signer = MoveSigner(MoveAddress(address_bytes));
+
+    let signer_address = copy_to_guest(&mut instance, &mut runtime.allocator, &move_signer)?;
+
     instance
-        .call_typed_and_get_result::<u32, ()>(&mut runtime, "main", ())
+        .call_typed_and_get_result::<(), (u32,)>(&mut runtime, "main", (signer_address,))
         .map_err(|e| anyhow::anyhow!("{e:?}"))?;
 
     Ok(())
